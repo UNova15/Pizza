ed0e741f0bba996b7ceaeaf9b8b34b1ebf2f1b98
Kirill <kkrecetov5@gmail.com>
2025-07-22 14:53:11 +0700

    Implemented a pizza constructor

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.util.LinkedList;
 import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 abstract class AbstractIngredient {
     private String name;
@@ -33,6 +36,10 @@ class Ingredient extends AbstractIngredient {
         super(name, price);
     }
 
+    Ingredient(String[] stringValues) {
+        super(stringValues[0], Double.parseDouble(stringValues[1]));
+    }
+
 }
 
 class PizzaBase extends AbstractIngredient {
@@ -41,6 +48,9 @@ class PizzaBase extends AbstractIngredient {
         super(name, price);
     }
 
+    PizzaBase(String[] stringValues) {
+        super(stringValues[0], Double.parseDouble(stringValues[1]));
+    }
 }
 
 class Pizza {
@@ -51,11 +61,11 @@ class Pizza {
     private LinkedList<Ingredient> ingredients;
     private PizzaBase pizzaBase;
 
-    Pizza(String name, PizzaBase pizzabase, Ingredient... ingredients) {
+    Pizza(String name, PizzaBase pizzabase, LinkedList<Ingredient> ingredients) {
         this.name = name;
         this.pizzaBase = pizzabase;
 
-        this.ingredients = new LinkedList<>(Arrays.asList(ingredients));
+        this.ingredients = ingredients;
         priceUpdate();
     }
 
@@ -90,10 +100,108 @@ class Pizza {
 }
 
 
+class Menu {
+    private LinkedList<Ingredient> pizzaIngredients = new LinkedList<>();
+    private LinkedList<PizzaBase> pizzaBases = new LinkedList<>();
+
+    Menu(String ingredientsFile, String basesFile) {
+        readIngredientsFromFile(ingredientsFile, basesFile);
+    }
+
+    void readIngredientsFromFile(String ingredientsFile, String basesFile) {
+        try {
+
+            Scanner scanner = new Scanner(new File(ingredientsFile));
+
+            while (scanner.hasNextLine()) {
+                pizzaIngredients.add(new Ingredient(scanner.nextLine().split(" ")));
+            }
+            scanner.close();
+
+
+            scanner = new Scanner(new File(basesFile));
+            while (scanner.hasNextLine()) {
+                pizzaBases.add(new PizzaBase(scanner.nextLine().split(" ")));
+            }
+            scanner.close();
+
+        } catch (FileNotFoundException exc) {
+            System.err.println("Ошибка чтения файла");
+        }
+    }
+
+    private <T extends AbstractIngredient> LinkedList<T> searchInMenu(LinkedList<T>listIngredients,String... ingredientsName){
+        LinkedList<T> ingredients = new LinkedList<>();
+
+        for (String i : ingredientsName) {
+            boolean flag = false;
+            for (T j : listIngredients) {
+                if (i.equals(j.getName())) {
+                    ingredients.add(j);
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag){
+                throw new IllegalArgumentException();
+            }
+        }
+
+        return ingredients;
+    }
+
+    LinkedList<Ingredient> getIngredients() {
+        return pizzaIngredients;
+    }
+
+    LinkedList<Ingredient> getIngredients(String... ingredientsName) {
+        return searchInMenu(pizzaIngredients,ingredientsName);
+    }
+
+
+    LinkedList<PizzaBase> getBases() {
+        return pizzaBases;
+    }
+
+    PizzaBase getBases(String basesName){
+        return searchInMenu(pizzaBases,basesName).getFirst();
+    }
+
+    void changeIngredient(String name,String newName,double newPrice){
+        Ingredient ingredient = searchInMenu(pizzaIngredients,name).getFirst();
+        ingredient.setName(newName);
+        ingredient.setPrice(newPrice);
+    }
+
+    void changeBase(String name, String newName,double newPrice){
+        PizzaBase base = searchInMenu(pizzaBases,name).getFirst();
+        base.setName(newName);
+        base.setPrice(newPrice);
+    }
+
+}
+
+class PizzaConstructor {
+    private String ingredientsFile;
+    private String basesFile;
+
+    private Menu menu;
+
+    PizzaConstructor(String ingredientsFile, String basesFile) {
+        this.basesFile = basesFile;
+        this.ingredientsFile = ingredientsFile;
+        this.menu = new Menu(ingredientsFile, basesFile);
+    }
+
+    Pizza createPizza(String pizzaName,String pizzaBaseName, String... ingredientsName) {
+        return new Pizza(pizzaName,menu.getBases(pizzaBaseName),menu.getIngredients(ingredientsName));
+    }
+
+}
 
 
 class Main {
     public static void main(String[] args) {
-
+        PizzaConstructor constructor = new PizzaConstructor("C:\\Users\\Kirill\\IdeaProjects\\Pizza\\src\\Ingredients.txt","C:\\Users\\Kirill\\IdeaProjects\\Pizza\\src\\Bases.txt");
     }
 }
